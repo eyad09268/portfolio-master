3d59dd5dfe6fbd8371d1a3794389f948544d9781
eyad09268 <eyad09268@gmail.com>
2024-09-05 19:27:10 +0300

    ok5

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -23,7 +23,7 @@ function Techstack() {
   return (
     <Row style={{justifyContent: 'center', paddingBottom: '50px'}}>
       <Col xs={4} md={2} className="tech-icons">
-        <FaHtml5 />
+        <CgCPlusPlus />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
